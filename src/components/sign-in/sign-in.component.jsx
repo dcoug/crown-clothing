a533fbfe2c2dab344da0ad8019cf00d1361ca090
D Coug <dlcougle@gmail.com>
2021-02-14 16:56:43 -0800

    added style-component jsx css

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
+import { SignInContainer, SignInTitle, ButtonBarContainer } from './sign-in.styles';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -35,8 +35,8 @@ class SignIn extends React.Component {
 
     render() {
         return (
-            <div className="sign-in">
-                <h2>I already have an account</h2>
+            <SignInContainer>
+                <SignInTitle>I already have an account</SignInTitle>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -56,15 +56,15 @@ class SignIn extends React.Component {
                         label="Password"
                         required
                     />
-                    <div className="buttons">
+                    <ButtonBarContainer>
                         <CustomButton type="submit">Sign In</CustomButton>
                         <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             {' '}
                         Sign in with Google{' '}
                         </CustomButton>
-                    </div>
+                    </ButtonBarContainer>
                 </form>
-            </div>
+            </SignInContainer>
         );
     }
 }
